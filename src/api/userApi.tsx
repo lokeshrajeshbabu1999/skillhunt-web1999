a6ec8aaf3ad9f97947eca370a70b60f586d9a679
Prasath Basuvaraj <prasath.buddie@gmail.com>
2023-02-25 18:31:01 -0500

    Formatting

--- src/api/userApi.tsx
+++ src/api/userApi.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-const USER_URL = "https://8xhttajyc8.execute-api.ap-south-1.amazonaws.com/Prod";
+import axios from 'axios';
+const USER_URL = 'https://8xhttajyc8.execute-api.ap-south-1.amazonaws.com/Prod';
 const userApi = axios.create({
   baseURL: USER_URL.trim(),
 });
