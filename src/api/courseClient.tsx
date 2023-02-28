080d768e9fd4a6bee332eb58c9fc5d035e123f84
lokesh <lokeshrajeshbabu28@gmail.com>
2023-02-28 08:02:29 +0530

    authorization

--- src/api/courseClient.tsx
+++ src/api/courseClient.tsx
@@ -1,33 +1,36 @@
 import { COURSE_API_URL } from '@env';
 import axios from 'axios';
-// import { Buffer } from 'buffer';
+
 console.log('Course API_URL', COURSE_API_URL);
 const courseClient = axios.create({
   baseURL: COURSE_API_URL.trim(),
 });
 
 courseClient.interceptors.request.use(request => {
-  // replace console with our logger of choice
-  // global.Buffer = Buffer;
-  // console.log('Request Base & Url : ', request.baseURL, request.url);
-  // const token = API_USERNAME + ':' + API_PASSWORD;
-  // const encodedToken = Buffer.from(token).toString('base64');
-  // request.headers.Authorization = 'Basic ' + encodedToken;
-  // console.log(request);
-  return request;
-});
-
-courseClient.interceptors.request.use(request => {
-  // replace console with our logger of choice
+  request.headers.Authorization = userAccessToken
   console.log(
     'Request Base & Url : ',
     request.baseURL,
     request.url,
     request.method,
+    request.headers
   );
-  // console.log(request);
+  console.log(request);
   return request;
 });
+
+// courseClient.interceptors.request.use(request => {
+// replace console with our logger of choice
+// console.log(
+//   'Request Base & Url : ',
+//   request.baseURL,
+//   request.url,
+//   request.method,
+//   request.headers
+// );
+// console.log(request);
+//   return request;
+// });
 
 courseClient.interceptors.response.use(
   response => {
--- src/api/userClient.tsx
+++ src/api/userClient.tsx
@@ -1,10 +1,26 @@
-import { USER_API_URL } from '@env';
+import { USER_API_URL, API_USERNAME, API_PASSWORD } from '@env';
 import axios from 'axios';
+import { Buffer } from "buffer";
 
-console.log('Course API_URL', USER_API_URL);
+console.log('User API_URL', USER_API_URL);
 const userClient = axios.create({
   baseURL: USER_API_URL.trim(),
 });
+
+userClient.interceptors.request.use(request => {
+
+  // replace console with our logger of choice
+  global.Buffer = Buffer;
+  console.log('Request Base & Url : ', request.baseURL, request.url);
+  const token = API_USERNAME + ':' + API_PASSWORD;
+  const encodedToken = Buffer.from(token).toString('base64');
+  // const header = `Authorization: Bearer ${token}`;
+  request.headers.Authorization = 'Basic ' + encodedToken;
+  console.log(request);
+  return request;
+});
+// const headers = { Authorization: `Bearer ${token}` };
+// return axios.get(URLConstants.USER_URL, { headers })
 
 userClient.interceptors.request.use(request => {
   // replace console with our logger of choice
