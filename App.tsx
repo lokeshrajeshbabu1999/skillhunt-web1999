49f270580677c99a16038e915f17bb65006aba50
Prasath Basuvaraj <prasath.buddie@gmail.com>
2023-05-03 23:30:25 -0400

    Environment

--- App.tsx
+++ App.tsx
@@ -1,89 +1,11 @@
-/* eslint-disable react/no-unstable-nested-components */
-import { Authenticator } from '@aws-amplify/ui-react-native';
-import { Amplify } from 'aws-amplify';
-import { ThemeProvider } from 'styled-components';
-
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import awsExports from './src/aws-exports';
-import { CourseNavigator } from './src/features/course/components/CourseStack';
-import { HomeNavigator } from './src/features/home/components/HomeStack';
-import UserProfile from './src/features/profile/screens/UserProfile';
-import UserSchedule from './src/features/schedule/screens/UserSchedule';
-import { lightTheme } from './src/theme';
-
-Amplify.configure(awsExports);
-
-const Tab = createMaterialBottomTabNavigator();
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
+import { Text } from '@rneui/base';
+import Config from 'react-native-config';
 
 function App(): JSX.Element {
-  // const isDarkMode = useColorScheme() === 'dark';
-
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
-
   return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <ThemeProvider theme={lightTheme}>
-          <NavigationContainer>
-            <Tab.Navigator
-              barStyle={{ backgroundColor: lightTheme.PRIMARY_COLOR }}>
-              <Tab.Screen
-                name="HomeStack"
-                component={HomeNavigator}
-                options={{
-                  tabBarLabel: 'Home',
-                  tabBarIcon: ({ color }) => (
-                    <Icon name="home" size={26} color={color} />
-                  ),
-                }}
-              />
-              <Tab.Screen
-                name="Course"
-                component={CourseNavigator}
-                options={{
-                  tabBarLabel: 'Courses',
-                  tabBarIcon: ({ color }) => (
-                    <Icon name="book-open" size={26} color={color} />
-                  ),
-                }}
-              />
-              <Tab.Screen
-                name="Schedule"
-                component={UserSchedule}
-                options={{
-                  tabBarLabel: 'Schedule',
-                  tabBarIcon: ({ color }) => (
-                    <Icon
-                      name="calendar-month-outline"
-                      size={26}
-                      color={color}
-                    />
-                  ),
-                }}
-              />
-              <Tab.Screen
-                name="Profile"
-                component={UserProfile}
-                options={{
-                  tabBarLabel: 'Account',
-                  tabBarIcon: ({ color }) => (
-                    <Icon name="account" size={26} color={color} />
-                  ),
-                }}
-              />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </ThemeProvider>
-      </Authenticator>
-    </Authenticator.Provider>
+    <>
+      <Text>Course API : {Config.COURSE_API_URL}</Text>
+    </>
   );
 }
 
--- env.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@env' {
-  export const COURSE_API_URL: string;
-  export const USER_API_URL: string;
-  export const IMAGE_URL: string;
-}
