2fe70a409319522eb399ed78fac4d263246ebe95
lokesh <lokeshrajeshbabu28@gmail.com>
2023-05-18 18:33:49 +0530

    new schedule added

---
+++ src/features/schedule/screens/GetSchedule.tsx
@@ -0,0 +1,16 @@
+import { Card } from "@rneui/themed";
+import { Text } from 'react-native';
+
+const GetSchedule = ({ schedule }) => {
+    return (
+        <Card>
+            <Text>user</Text>
+            <Text>{schedule.course_id}</Text>
+            <Text>{schedule.user_schedule_id}</Text>
+            <Text>{schedule.user_id}</Text>
+            <Text>{schedule.schedule_id}</Text>
+            <Text>{schedule.created_ts}</Text>
+        </Card>
+    );
+};
+export default GetSchedule;
