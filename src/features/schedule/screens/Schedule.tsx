b290b1f819b652d53c5ae79722dc3ee7741cfeb1
lokesh <lokeshrajeshbabu28@gmail.com>
2023-06-03 11:18:10 +0530

    component added once again

---
+++ src/features/schedule/screens/Schedule.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+const Schedule = ({ schedule }) => {
+    return (
+        <View>
+            <Text>{schedule.id}</Text>
+            <Text>{schedule.courseid}</Text>
+            <Text>{schedule.userid}</Text>
+            <Text>{schedule.userscheduleid}</Text>
+            <Text>{schedule.scheduleid}</Text>
+        </View>
+    );
+};
+
+export default Schedule;
