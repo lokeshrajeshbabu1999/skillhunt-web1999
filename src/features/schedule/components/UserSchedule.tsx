000fa1683f87fa75de573bf0ffc801e9b2d7b2ac
lokesh <lokeshrajeshbabu28@gmail.com>
2023-07-04 17:41:49 +0530

    card styled

--- src/features/schedule/components/UserSchedule.tsx
+++ src/features/schedule/components/UserSchedule.tsx
@@ -1,16 +1,33 @@
 import { Card } from '@rneui/themed';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { useTheme } from 'styled-components';
 
 const UserSchedule = ({ schedule, course }) => {
+  const theme = useTheme();
+
   return (
-    <Card>
-      {/* <Text>{course.author}</Text> */}
-      <Text>{schedule.course_id}</Text>
-      <Text>{schedule.user_schedule_id}</Text>
-      <Text>{schedule.user_id}</Text>
-      <Text>{schedule.schedule_id}</Text>
-      <Text>{schedule.created_ts}</Text>
-    </Card>
+    <View style={styles.card_template}>
+      <Card>
+        {/* <Text>{course.author}</Text> */}
+        <Text>{schedule.course_id}</Text>
+        <Text>{schedule.user_schedule_id}</Text>
+        <Text>{schedule.user_id}</Text>
+        <Text>{schedule.schedule_id}</Text>
+        <Text>{schedule.created_ts}</Text>
+      </Card>
+    </View>
   );
 };
 export default UserSchedule;
+
+const styles = StyleSheet.create({
+  card_template: {
+    aspectRatio: 2.2,
+    width: 270,
+    height: 180,
+    justifyContent: "flex-start",
+    padding: 16,
+    borderRadius: 30,
+    backgroundColor: "orange",
+  },
+})
