4f3d74322f7ccaaf68216f8a16e313cbfbd7cf68
lokesh <lokeshrajeshbabu28@gmail.com>
2023-07-06 07:36:55 +0530

    course expanded

--- src/features/schedule/components/UserSchedule.tsx
+++ src/features/schedule/components/UserSchedule.tsx
@@ -1,33 +1,36 @@
 import { Card } from '@rneui/themed';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { CourseAuthor, CourseTitle } from '../../../../style';
 import shLogger from '../../../utils/Loggers';
 
 const UserSchedule = ({ schedule }) => {
   shLogger.debug('Schedule when in UserSchedule component ', schedule);
 
+  const { course } = schedule
   return (
-    <View style={styles.card_template}>
-      <Card>
-        <Text>{schedule.course.author}</Text>
-        <Text>{schedule.course_id}</Text>
-        <Text>{schedule.user_schedule_id}</Text>
-        <Text>{schedule.user_id}</Text>
-        <Text>{schedule.schedule_id}</Text>
-        <Text>{schedule.created_ts}</Text>
-      </Card>
-    </View>
+    // <View style={styles.card_template}>
+    <Card>
+      <CourseTitle>{course.title}</CourseTitle>
+      <CourseAuthor>{course.author}</CourseAuthor>
+      <Text>{schedule.course_id}</Text>
+      <Text>{schedule.user_schedule_id}</Text>
+      <Text>{schedule.user_id}</Text>
+      <Text>{schedule.schedule_id}</Text>
+      <Text>{schedule.created_ts}</Text>
+    </Card>
+    // </View>
   );
 };
 export default UserSchedule;
 
-const styles = StyleSheet.create({
-  card_template: {
-    aspectRatio: 2.2,
-    width: 270,
-    height: 180,
-    justifyContent: 'flex-start',
-    padding: 16,
-    borderRadius: 30,
-    backgroundColor: 'orange',
-  },
-});
+// const styles = StyleSheet.create({
+//   card_template: {
+//     aspectRatio: 2.2,
+//     width: 270,
+//     height: 180,
+//     justifyContent: 'flex-start',
+//     padding: 16,
+//     borderRadius: 30,
+//     backgroundColor: 'orange',
+//   },
+// });
